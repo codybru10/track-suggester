234eeeb29f27e1bf8d5055a4706ee5f392763b2e
Cody <cody.brubaker@gmail.com>
2016-10-20 20:42:47 -0700

    add base html and js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  debugger;
+  $("form").submit(function(event){
+  var q1 = parseInt($("#q1").val());
+  var q2 = parseInt($("#q2").val());
+  var q3 = parseInt($("#q3").val());
+  var q4 = parseInt($("#q4").val());
+  var q5 = parseInt($("#q5").val());
+  var result = q1 + q2 + q3 + q4 + q5
+
+  if (result <= 5){
+    $(".ruby").show();
+  } else if (result > 4 && result < 8){
+    $(".c").show();
+  } else if (result > 7){
+    $(".design").show();
+  }
+
+  event.preventDefault();
+  });
+
+
+});
